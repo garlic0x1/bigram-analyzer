863f0cf16e6058cc94f76c28746be04084bcd1e4
garlic0x1 <garlic1@protonmail.com>
2022-06-25 15:46:32 -0700

    bug fixes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::stdin;
 use std::collections::HashMap;
+use std::io::stdin;
 use utf8_chars::BufReadCharsExt;
 
 static SET: &str = "abcdefghijklmnopqrstuvwxyz1234567890";
@@ -8,26 +8,28 @@ static BREAK: &str = "\t\n !@#$%^&*()+=[]{}\\|;:'\"/?><,.`~";
 fn main() {
     let mut matrix: HashMap<char, HashMap<char, u32>> = HashMap::new();
     for i in SET.chars() {
-    let mut inner: HashMap<char, u32> = HashMap::new();
-    for j in SET.chars() {
-        inner.insert(j, 0);
-    }
+        let mut inner: HashMap<char, u32> = HashMap::new();
+        for j in SET.chars() {
+            inner.insert(j, 0);
+        }
         matrix.insert(i, inner);
     }
-    println!("{:?}", matrix);
     let mut last: Option<char> = None;
     for c in stdin().lock().chars().map(|x| x.unwrap()) {
         if BREAK.contains(c) {
-            
             last = None;
             continue;
         }
-        if let Some(l) = last {
-            println!("accessing: {}, {}", l, c);
-            let cell = matrix.get_mut(&l).expect("no row").get_mut(&c).expect("no cell");
-            *cell += 1;
-        }
         if SET.contains(c) {
+            if let Some(l) = last {
+                println!("accessing: {}, {}", l, c);
+                let cell = matrix
+                    .get_mut(&l)
+                    .expect("no row")
+                    .get_mut(&c)
+                    .expect("no cell");
+                *cell += 1;
+            }
             last = Some(c);
         }
         println!("{}", c);
